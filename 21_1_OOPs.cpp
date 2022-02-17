02393137bdae2ad4ffc8adf146e8fd15d169950f
Swastik  Dubey <89216281+Swastik-Dubey@users.noreply.github.com>
2022-02-17 20:56:59 +0530

    enter

--- 21_1_OOPs.cpp
+++ 21_1_OOPs.cpp
@@ -7,12 +7,22 @@ class student{
     int age;
     bool gender;
 
-    void printInfo(){}
+    void printInfo(){
+        cout<<"Name = ";
+        cout<<name<<endl;
+        cout<<"Age = ";
+        cout<<age<<endl;
+        cout<<"Gender = ";
+        cout<<gender<<endl;
+    }
 
 
 };
 
 int main(){
     student arr[3];
+    for(int i=0;i<3;i++){
+
+    }
     return 0;
 }
