c718b42b678029ea5680d7fc0b2e0cd64541c772
Swastik  Dubey <89216281+Swastik-Dubey@users.noreply.github.com>
2022-02-18 15:52:55 +0530

    .

--- 21_1_constructor.cpp
+++ 21_1_constructor.cpp
@@ -4,3 +4,29 @@ using namespace std;
 class student{
     string name;
     public:
+    int age;
+    bool gender;
+
+    student(string s, int a,int g){
+        name =s;
+        age=a;
+        gender=g;
+
+    }
+
+    void printInfo(){
+        cout<<"Name = ";
+        cout<<name<<endl;
+        cout<<"Age = ";
+        cout<<age<<endl;
+        cout<<"Gender = ";
+        cout<<gender<<endl;
+    }
+
+};
+
+int main(){
+    student a("Urvi",20,1);
+
+    return 0;
+}
