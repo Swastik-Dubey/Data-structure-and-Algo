4d4e77bd56b6d10d1f74c31bb561a9ee900aca1e
Swastik  Dubey <89216281+Swastik-Dubey@users.noreply.github.com>
2022-03-19 15:57:48 +0530

    .

--- prac.cpp
+++ prac.cpp
@@ -1,28 +1,10 @@
 #include<iostream>
 using namespace std;
 
-class emp {
-    int a;
-    public:
-    char b;
-    int c=a;
-    int d;
-    int input(int a, int d){
-        cout<<a+d;
 
-    }
 
-};
 int main(){
-    emp e1;
-    cin>>e1.b;
-    cin>>e1.c;
-    cin>>e1.d;
-    e1.input(e1.c, e1.d);
-  
-
-    
- 
+    cout<<"Welcome"<<endl;
  
      return 0;
 }
